dab4f3b27215b818e49ed99a0f9c22ca5d31aa1c
Martin Garcia <newluxfero@gmail.com>
2018-06-17 10:48:27 +0200

    First pokemon view

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -31,7 +31,41 @@ class Pokemon extends Component {
   }
 
   render() {
-    return null;
+    return (
+      <div className="columns">
+        <div className="column">
+          <figure className="image is-128x128">
+            <img src={this.state.sprites.front_default} alt={this.state.name} />
+          </figure>
+        </div>
+        <div className="column is-three-quarters">
+          <div className="content">
+            <h1 style={{ textTransform: "capitalize" }}>{this.state.name}</h1>
+
+            <h2>Pokédex data</h2>
+            <table className="table is-hoverable">
+              <tr>
+                <td>National №</td>
+                <td>{this.state.order}</td>
+              </tr>
+              <tr>
+                <td>Type</td>
+                <td>{JSON.stringify(this.state.types)}</td>
+              </tr>
+              <tr>
+                <td>Height</td>
+                <td>{this.state.height}</td> {/*2′4″ (0.71m)*/}
+              </tr>
+              <tr>
+                <td>Weight</td>
+                <td>{this.state.weight}</td>
+                {/*15.2 lbs (6.9 kg)*/}
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
