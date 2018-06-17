839d3cd77bb058c8f3726b67438c952eebb283e6
Martin Garcia <newluxfero@gmail.com>
2018-06-17 09:50:23 +0200

    Create first component

---
+++ src/components/HelloWorldClass.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class HelloWorldClass extends Component {
+  render() {
+    return <div>Hello world from a class!</div>;
+  }
+}
+
+export default HelloWorldClass;
